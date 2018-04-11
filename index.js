9bba9f980867c76863588b382a6c09e1aefbad07
Ben Gourley <bn@grly.me>
2018-04-11 16:53:49 +0100

    Create @bugsnag/safe-json-stringify fork
    - Add MAX_DEPTH, MAX_EDGES and MIN_PRESERVED_DEPTH features
    - Add standard linting
    - Convert tests to use karma so that we can test in browsers
    - Add tests for new features
    - Replace features that IE8 doesn't have (Array.indexOf etc.)
    - Update readme
    - Update package.json

--- index.js
+++ index.js
@@ -1,65 +1,91 @@
-var hasProp = Object.prototype.hasOwnProperty;
-
-function throwsMessage(err) {
-	return '[Throws: ' + (err ? err.message : '?') + ']';
+module.exports = function (data, replacer, space) {
+  return JSON.stringify(ensureProperties(data), replacer, space)
 }
 
-function safeGetValueFromPropertyOnObject(obj, property) {
-	if (hasProp.call(obj, property)) {
-		try {
-			return obj[property];
-		}
-		catch (err) {
-			return throwsMessage(err);
-		}
-	}
+var MAX_DEPTH = 20
+var MAX_EDGES = 10000
+var MIN_PRESERVED_DEPTH = 10
 
-	return obj[property];
+var MAX_EDGES_EXCEEDED_NODE = '[MAX_EDGES exceeded]'
+var MAX_DEPTH_EXCEEDED_NODE = '[MAX_DEPTH exceeded]'
+
+function throwsMessage (err) {
+  return '[Throws: ' + (err ? err.message : '?') + ']'
 }
 
-function ensureProperties(obj) {
-	var seen = [ ]; // store references to objects we have seen before
-
-	function visit(obj) {
-		if (obj === null || typeof obj !== 'object') {
-			return obj;
-		}
-
-		if (seen.indexOf(obj) !== -1) {
-			return '[Circular]';
-		}
-		seen.push(obj);
-
-		if (typeof obj.toJSON === 'function') {
-			try {
-				var fResult = visit(obj.toJSON());
-				seen.pop();
-				return fResult;
-			} catch(err) {
-				return throwsMessage(err);
-			}
-		}
-
-		if (Array.isArray(obj)) {
-			var aResult = obj.map(visit);
-			seen.pop();
-			return aResult;
-		}
-
-		var result = Object.keys(obj).reduce(function(result, prop) {
-			// prevent faulty defined getter properties
-			result[prop] = visit(safeGetValueFromPropertyOnObject(obj, prop));
-			return result;
-		}, {});
-		seen.pop();
-		return result;
-	};
-
-	return visit(obj);
+function find (haystack, needle) {
+  for (var i = 0, len = haystack.length; i < len; i++) {
+    if (haystack[i] === needle) return true
+  }
+  return false
 }
 
-module.exports = function(data, replacer, space) {
-	return JSON.stringify(ensureProperties(data), replacer, space);
+function isArray (obj) {
+  return Object.prototype.toString.call(obj) === '[object Array]'
 }
 
-module.exports.ensureProperties = ensureProperties;
+function safelyGetProp (obj, prop) {
+  try {
+    return obj[prop]
+  } catch (err) {
+    return throwsMessage(err)
+  }
+}
+
+function ensureProperties (obj) {
+  var seen = [] // store references to objects we have seen before
+  var edges = 0
+
+  function visit (obj, depth) {
+    function edgesExceeded () {
+      return depth > MIN_PRESERVED_DEPTH && edges > MAX_EDGES
+    }
+
+    if (depth === undefined) depth = 0
+    if (depth > MAX_DEPTH) return MAX_DEPTH_EXCEEDED_NODE
+    if (edgesExceeded()) return MAX_EDGES_EXCEEDED_NODE
+    if (obj === null || typeof obj !== 'object') return obj
+    if (find(seen, obj)) return '[Circular]'
+
+    seen.push(obj)
+
+    if (typeof obj.toJSON === 'function') {
+      try {
+        var fResult = visit(obj.toJSON(), depth)
+        seen.pop()
+        return fResult
+      } catch (err) {
+        return throwsMessage(err)
+      }
+    }
+
+    if (isArray(obj)) {
+      var aResult = []
+      for (var i = 0, len = obj.length; i < len; i++) {
+        edges++
+        if (edgesExceeded()) {
+          aResult.push(MAX_EDGES_EXCEEDED_NODE)
+          break
+        }
+        aResult.push(visit(obj[i], depth + 1))
+      }
+      seen.pop()
+      return aResult
+    }
+
+    var result = {}
+    for (var prop in obj) {
+      if (!Object.prototype.hasOwnProperty.call(obj, prop)) continue
+      edges++
+      if (edgesExceeded()) {
+        result[prop] = MAX_EDGES_EXCEEDED_NODE
+        break
+      }
+      result[prop] = visit(safelyGetProp(obj, prop), depth + 1)
+    }
+    seen.pop()
+    return result
+  }
+
+  return visit(obj)
+}
